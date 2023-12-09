1990ed7fe4a9afbe173760d7477a0cbaf880122d
masakinihirota <masakinihirota@gmail.com>
2023-12-09 22:38:28 +0900

    ⚙ Update package.json, _meta.json, index.mdx, and theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>masakinihirota</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/masakinihirota/masakinihirota',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/masakinihirota/masakinihirota_doc',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'masakinihirota',
   },
+  darkMode: true,
 }
 
 export default config
